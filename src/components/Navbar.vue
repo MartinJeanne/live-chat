9ed6b63225ee821322c9c8d3d01069953f9f7bb8
MartinJeanne <martinjeanne.dev@gmail.com>
2022-01-18 11:01:35 +0100

    User can now logout + the website can now see the current user

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,36 +1,50 @@
 <template>
-  <nav>
-      <div>
-          <p>Hey there... display name here</p>
-          <p class="email">currentrly logged in as... email</p>
-      </div>
-      <button>Logout</button>
+  <nav v-if="user">
+    <div>
+      <p>{{ user.displayName }}</p>
+      <p class="email">{{ user.email }}</p>
+    </div>
+    <button @click="clickLogout">Logout</button>
   </nav>
 </template>
 
 <script>
-export default {
+import useLogout from '../composables/useLogout'
+import getUser from '../composables/getUser'
 
+export default {
+  setup() {
+    const { error, logout } = useLogout()
+    const { user } = getUser()
+
+    async function clickLogout() {
+        await logout()
+        if (!error.value) {
+            console.log("Logout user")
+        }
+    }
+    return { clickLogout, user }
+  }
 }
 </script>
 
 <style>
 nav {
-    padding: 20px;
-    border-bottom: 1px solid rgb(66, 66, 66);
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  padding: 20px;
+  border-bottom: 1px solid rgb(66, 66, 66);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
 nav p {
-    margin: 2px auto;
-    font-size: 16px;
-    color: rgb(232, 234, 235);
+  margin: 2px auto;
+  font-size: 16px;
+  color: rgb(232, 234, 235);
 }
 
 nav p.email {
-    font-size: 14px;
-    color: rgb(209, 209, 209);
+  font-size: 14px;
+  color: rgb(209, 209, 209);
 }
 </style>
